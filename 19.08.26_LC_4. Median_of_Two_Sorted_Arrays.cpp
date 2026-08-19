161f154787283573fea88fda854ac33465883829
dakshpanchal1419 <dakshpanchal1419@gmail.com>
2026-08-19 13:58:09 +0530

    Create 19.08.26_LC_4. Median_of_Two_Sorted_Arrays.cpp

---
+++ 19.08.26_LC_4. Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,33 @@
+
+
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2) {
+        int i=0;
+            int j=0;
+            float median;
+            int result =0;
+         while (nums1[i]<nums2[j]){
+            break;
+            i++;
+         }
+         for (j =0 ; j< n ;j++){
+            nums1.push_back(num2[j])
+         }
+         //code for median 
+         for (int i=0 ; i< m ; i++){
+            if (m%2==0){
+              median =(nums[m/2] + nums[(m/2)+1])/2;
+            }
+           else{
+             median 
+             return 
+           }
+             return median ;
+         }
+        
+
+         
+     };
