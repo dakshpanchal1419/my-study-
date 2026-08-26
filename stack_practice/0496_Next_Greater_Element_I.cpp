8ebee92fc31942cb6cf23d3e86ab22aa84125acd
dakshpanchal1419 <dakshpanchal1419@gmail.com>
2026-08-26 09:40:45 +0530

    Add comments for clarity in Next Greater Element I
    Add comments to clarify the approach for finding the next greater element.

---
+++ stack_practice/0496_Next_Greater_Element_I.cpp
@@ -0,0 +1,11 @@
+// --cleaner way to think about the problem---
+//   nums2
+//  ↓
+// process elements from left → right
+//  ↓
+// maintain candidates
+//  ↓
+// when current element is greater
+//  ↓
+// resolve previous elements
+
